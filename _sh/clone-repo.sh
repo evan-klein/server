c0b4c6ca2232ae73c0e4831c27a624f9b3f3d7b6
Evan Klein <me@evanklein.com>
2024-07-07 08:21:54 -0700

    sl: progress commit of clone-repo.sh

--- _sh/clone-repo.sh
+++ _sh/clone-repo.sh
@@ -13,3 +13,21 @@ github_username="$1"
 github_repo_name="$2"
 
 tmp_dir=`mktemp -d`
+
+# Clone repo to temporary directory
+if git clone --quiet git@github.com:$github_username/$github_repo_name.git $tmp_dir > /dev/null 2>&1; then
+	echo '✅ Successfully clone repo via SSH\n'
+else
+	if git clone --quiet https://github.com/$github_username/$github_repo_name.git $tmp_dir > /dev/null 2>&1; then
+		echo '✅ Successfully clone repo via HTTPS\n'
+	else
+		echo '⚠️ Fatal error: failed to clone repo via SSH/HTTPS\n'
+		exit 1
+	fi
+fi
+
+# Get the last commit ID
+last_commit_id=`git -C $tmp_dir rev-parse HEAD`
+
+# Get current datetime
+timestamp=`date +%Y-%m-%d-%H-%M-%S`
