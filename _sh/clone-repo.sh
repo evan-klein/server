874b142bb09b261397a43cd8d239a063b8e827af
Evan Klein <me@evanklein.com>
2024-07-07 07:57:59 -0700

    sl: progress commit of clone-repo.sh

--- _sh/clone-repo.sh
+++ _sh/clone-repo.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-# TODO
+# Check for required arguments
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo '⚠️ Fatal error: required arguments missing\n'
+	echo 'Usage:'
+	echo '\tsh clone-repo.sh <github_username> <github_repo_name>'
+	echo '\tsh clone-repo.sh evan-klein server'
+	exit 1
+fi
+
+github_username="$1"
+github_repo_name="$2"
+
+tmp_dir=`mktemp -d`
