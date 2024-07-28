c4d689bf78baf68de4129bcdf466bdcf89f10dac
Evan Klein <me@evanklein.com>
2024-07-28 10:05:37 -0700

    sl: progress commit of unattended upgrades config

--- _sh/build/cfg-defaults.sh
+++ _sh/build/cfg-defaults.sh
@@ -6,6 +6,9 @@ fqdn="$hostname.example.com"
 time_zone='America/Los_Angeles'
 default_editor='/bin/nano'
 
+apt_daily_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily.timer'
+apt_daily_upgrade_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily-upgrade.timer'
+
 supervisor_enabled=true
 caddy_enabled=true
 nodejs_enabled=true
