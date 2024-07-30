c1ce1a0e0ecb2b66c89eebfd2f33161c70626075
Evan Klein <me@evanklein.com>
2024-07-30 10:44:17 -0700

    sl: add stuff to reset-perms.sh

--- _sh/reset-perms.sh
+++ _sh/reset-perms.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-# TODO
+# Constants
+user="devops"
+home="/home/$user"
+ssh="$home/.ssh"
+ssh_config="$ssh/config"
+
+# SSH directory
+sudo chmod -Rf 700 $ssh
+sudo chown -Rf $user:$user $ssh
+
+# SSH config
+sudo chmod -f 700 $ssh_config
+sudo chown -f $user:$user $ssh_config
