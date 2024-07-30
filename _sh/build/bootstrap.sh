657ebd3600301cbe67a8f3d52b3037b441202e40
Evan Klein <me@evanklein.com>
2024-07-30 13:38:42 -0700

    sl: pc: bootstrap.sh

--- _sh/build/bootstrap.sh
+++ _sh/build/bootstrap.sh
@@ -2,6 +2,7 @@
 
 user='devops'
 ssh="/home/$user/.ssh"
+log='/var/log/evan-klein'
 
 # Install updates
 sudo apt-get -o Acquire::ForceIPv4=true update
@@ -12,3 +13,17 @@ sudo apt-get -y install git
 
 # Create user
 sudo adduser --gecos "DevOps,,," $user
+echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$user
+
+# Make directories and files
+mkdir $ssh/
+mkdir -p /etc/evan-klein/server/
+mkdir $log/
+touch $log/server.log
+touch $log/bootstrap.log
+touch $log/build.log
+
+# Set permissions
+curl --silent --show-error https://raw.githubusercontent.com/evan-klein/server/master/_sh/reset-perms.sh > ~/reset-perms.sh
+sh ~/reset-perms.sh
+rm ~/reset-perms.sh
