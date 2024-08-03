7f702dbe31287d17e86c15b8752607353ac2990c
Evan Klein <me@evanklein.com>
2024-08-03 12:32:31 -0700

    sl: reorganize to match build script order

--- _sh/build/cfg-defaults.sh
+++ _sh/build/cfg-defaults.sh
@@ -9,13 +9,13 @@ default_editor='/bin/nano'
 fail2ban_enabled=true
 fail2ban_jails='/usr/local/lib/evan-klein/server/cfg/fail2ban/*.local'
 
-apt_daily_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily.timer'
-apt_daily_upgrade_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily-upgrade.timer'
-
 supervisor_enabled=true
 caddy_enabled=true
 nodejs_enabled=true
 utilities_enabled=true
+
+apt_daily_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily.timer'
+apt_daily_upgrade_timer='/usr/local/lib/evan-klein/server/cfg/systemd/apt-daily-upgrade.timer'
 
 evan_bot_auto_reboot_enabled=true
 evan_bot_auto_reboot_cfg='/usr/local/lib/evan-klein/evan-bot-auto-reboot/cfg.json'
